da6021009ce1ff1bc77a42d3e11f7ec2fcf557bc
lubonch <lucasbonch@gmail.com>
2024-10-11 12:33:03 -0300

    Files deleted

--- MangaCount.Server/DTO/MangaDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MangaCountServer.DTO
-{
-    public class MangaDTO
-    {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public int? Volumes { get; set; }
-    }
-}
