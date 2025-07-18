4f4bf31a212e2f48b89438e4de06b34c9ef76c6a
lubonch <lucasbonch@gmail.com>
2025-07-18 17:17:10 -0300

    changes

--- MangaCount.Server/obj/Debug/net8.0/MangaCount.Server.AssemblyInfo.cs
+++ MangaCount.Server/obj/Debug/net8.0/MangaCount.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MangaCount.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ef0dca47dbb119c2d21a6cda02666e6f9484724")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18d1edf2e4c1aa73438534de10b9297d453dc0b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MangaCount.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MangaCount.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
