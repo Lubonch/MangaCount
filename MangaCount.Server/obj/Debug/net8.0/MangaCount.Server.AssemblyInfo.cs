371f30964f16b26a5396b74309f96b63d4f8686e
lubonch <lucasbonch@gmail.com>
2025-07-18 19:19:58 -0300

    git ignore upload

--- MangaCount.Server/obj/Debug/net8.0/MangaCount.Server.AssemblyInfo.cs
+++ MangaCount.Server/obj/Debug/net8.0/MangaCount.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MangaCount.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66abd51af61c03149dda17b532ecde3257eceffc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MangaCount.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MangaCount.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
